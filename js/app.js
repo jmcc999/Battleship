529c0ba861f039e9850c5e86092a1d5511075048
Joseph McClellan <josephmcclellan999@gmail.com>
2022-07-13 18:01:33 -0400

    set grids

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 console.log("you sunck my battleship!")
+
+
+
+//set grid size variables
+
+let p1gridSize = prompt("select grid size")//bigger grid = higher difficulty
+let p2gridSize =prompt("select grid size")
+//write an array of an arrays
+
+const createGrid = (size) =>
+{
+  let grid = []
+  for (let i = 0; i < size; i++){//change to for of loops?
+    grid[i] = []
+    for (let j=0; j < size; j++){
+      grid[i][j] = "-"
+    }
+  }
+  return grid
+}
+console.log(grid)
+//focus on MVP
